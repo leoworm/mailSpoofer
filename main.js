b4b9781e45ce7cd5bce154c7a3a1f77d51911fe1
leoworm <leo.charles.p@gmail.com>
2018-11-13 18:16:54 +0100

    Added Template functionality

--- main.js
+++ main.js
@@ -1,58 +1,65 @@
 var nodemailer = require("nodemailer");
 var prompt = require("prompt-sync")();
 var json = require("jsonfile");
+var fs = require('fs');
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
+//I don't think this is needed as I am hosting my own mail server
 nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'localhost',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'wini.ott@web.de', // generated ethereal user
-            pass: 'artemisfowl' // generated ethereal password
+
+  var tempIf = prompt("Use a template?(y/N) ");
+  if(tempIf.toLowerCase() !== "n"){
+    var templates = [];
+    fs.readdirSync('./templates/').forEach(file => {
+      templates.push(file);
+    })
+    var i = 0;
+    templates.forEach(function(file){
+      console.log(i + ': ' + file);
+      i++
+    })
+    var fileName = prompt('Please select template number: ');
+    var data = json.readFileSync('./templates/' + templates[fileName]);
+  }else{
+    var data = json.readFileSync('./templates/default.json');
+    data.mail.sender = prompt("Sender: ");
+    console.log();
+    data.mail.recipients = prompt("Recipient: ");
+    console.log();
+    data.mail.subject = prompt("Subject: ");
+    console.log();
+    data.mail.content = prompt("Content: ");
+    console.log();
+    data.mailAmount = prompt("Amount of mails: ");
+    console.log();
+  }
+
+  // setup email data with unicode symbols
+  let transporter = nodemailer.createTransport({
+    host: data.server.host,
+    port: data.server.post,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: data.server.auth.user, // generated ethereal user
+      pass: data.server.auth.pass // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  for(var j = 0; j < data.mail.recipients.length; j++){
+    let mailOptions = {
+      from: data.mail.sender, // sender address
+      to: data.mail.recipients[j], // list of receivers
+      subject: data.mail.subject, // Subject line
+      text: data.mail.content, // plain text body
+      html: '' // html body
+    };
+    for(var i = 0;i<data.mailAmount;i++){
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
         }
-    });
-	var tempIf = prompt("Use a template?(y/N)");
-	if(tempIf.toLowerCase() !== "n"){
-		console.log("Selected template.");
-		
-	}else{
-		var sender = prompt("Sender: ");
-		console.log();
-		var recipient = prompt("Recipient: ");
-		console.log();
-		var subj = prompt("Subject: ");
-		console.log();
-		var content = prompt("Content: ");
-		console.log();
-		var times = prompt("Amount of mails: ");
-		console.log();
-		
-		// setup email data with unicode symbols
-		let mailOptions = {
-			from: sender, // sender address
-			to: recipient, // list of receivers
-			subject: subj, // Subject line
-			text: content, // plain text body
-			html: '' // html body
-		};
-
-		// send mail with defined transport object
-		for(i=0;i<times;i++){
-			transporter.sendMail(mailOptions, (error, info) => {
-				if (error) {
-					return console.log(error);
-				}
-				console.log('Message sent: %s', info.messageId);
-				// Preview only available when sending through an Ethereal account
-				//console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-				// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-				// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-			});
-		};
-	};
+        console.log('R: ${j} of ${data.mail.recipients.length}, ${i} of ${data.mailAmount} // Message sent: ' + info.messageId + ' to ' + mailOptions.to);
+      });
+    };
+  };
 });
